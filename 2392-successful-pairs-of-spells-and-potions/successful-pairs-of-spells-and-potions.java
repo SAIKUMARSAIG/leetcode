a2c121aa712b0fdd591286ff54a3a9aeb2d27e8f
SAIKUMARGUDIPATI <155902731+SAIKUMARSAIG@users.noreply.github.com>
2026-05-24 18:46:16 +0530

    Time: 54 ms (16.80%) | Memory: 146.8 MB (72.83%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.java
@@ -18,37 +18,29 @@ class Solution {
 
 
 public static int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int n = spells.length;
+        int m = potions.length;
+        Arrays.sort(potions);
+        int res[] = new int[n];
+        int idx = 0;
+        for (int i = 0; i<n; i++){
+            int num = spells[i];
+            int l=0,r=m-1;
+            int ans = m;
+            while (l<=r){
+                int mid = l + (r-l)/2;
+                // int product = spells[i] * potions[mid];
+                long product = (long) spells[i] * potions[mid];
 
-    Arrays.sort(potions);
-
-    int n = spells.length;
-    int m = potions.length;
-
-    int[] res = new int[n];
-
-    for (int i = 0; i < n; i++) {
-
-        int left = 0;
-        int right = m - 1;
-        int ans = m;
-
-        while (left <= right) {
-
-            int mid = left + (right - left) / 2;
-
-            long product = (long) spells[i] * potions[mid];
-
-            if (product >= success) {
-                ans = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+                if (product>=success){
+                    ans = mid;
+                    r = mid-1;
+                } else if (product<success) {
+                    l = mid+1;
+                }
             }
+            res[i] = m-ans;
         }
-
-        res[i] = m - ans;
+        return res;
     }
-
-    return res;
-}
 }
